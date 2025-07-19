2a95831816a9e21990623317d2214640b3d75598
Eddy Ckedar <eddykedar@gmail.com>
2025-07-19 13:20:38 +0200

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # Chats app routes
+
+    # Your app API routes under /api/
+    path('api/', include('chats.urls')),
+
+    # DRF's login/logout views for the browsable API
+    path('api-auth/', include('rest_framework.urls')),
 ]
