8b623a5c99b30322f97b28ce7bdba07524e30f49
Eddy Ckedar <eddykedar@gmail.com>
2025-07-19 13:18:31 +0200

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,10 +1,39 @@
-from rest_framework.views import APIView
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
-from .models import Message
-from .serializers import MessageSerializer
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
-class MessageListView(APIView):
-    def get(self, request):
-        messages = Message.objects.all()
-        serializer = MessageSerializer(messages, many=True)
-        return Response(serializer.data)
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
+
+    def create(self, request, *args, **kwargs):
+        # Create new conversation with participants
+        participants = request.data.get('participants', [])
+        if not participants:
+            return Response({"error": "Participants list is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participants)
+        conversation.save()
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Create new message linked to a conversation and sender
+        data = request.data.copy()
+        data['sender'] = request.user.pk  # assumes authenticated user
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
