2bb970227918078d4fc46ad1d9268ed516e3a4ea
Eddy Ckedar <eddykedar@gmail.com>
2025-07-19 13:09:11 +0200

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -10,3 +10,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'chats',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
+
