4491574d7c70b2b9c2ce8ccb79ea28a356474043
Eddy Ckedar <eddykedar@gmail.com>
2025-08-02 00:37:49 +0200

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -5,12 +5,13 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.decorators.cache import cache_page
 from .models import Message
+from .managers import UnreadMessagesManager  # Ensure this is imported
 
 @cache_page(60)
 @login_required
 def message_list_view(request):
-    messages_list = Message.objects.filter(receiver=request.user).select_related('sender').prefetch_related('replies')
-    sent_messages = Message.objects.filter(sender=request.user).select_related('receiver').prefetch_related('replies')
+    messages_list = Message.unread.unread_for_user(request.user).only('id', 'content', 'sender', 'created_at').select_related('sender').prefetch_related('replies')
+    sent_messages = Message.objects.filter(sender=request.user).only('id', 'content', 'receiver', 'created_at').select_related('receiver').prefetch_related('replies')
     return render(request, 'messaging/message_list.html', {
         'messages': messages_list,
         'sent_messages': sent_messages
